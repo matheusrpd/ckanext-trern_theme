ef3926274a518ce5721f2c9563ecce8f2059bb70
matheusrpd <matheusrpd@gmail.com>
2021-11-08 18:19:14 -0300

    Adicionando header e arquivo js para controlar nav mobile.

--- ckanext/trern_theme/plugin.py
+++ ckanext/trern_theme/plugin.py
@@ -2,20 +2,22 @@ import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
 def get_groups():
-    groups = toolkit.get_action('group_list')(data_dict={'all_fields': True})
+  groups = toolkit.get_action('group_list')(data_dict={'all_fields': True})
 
-    return groups
+  return groups
 
 class TrernThemePlugin(plugins.SingletonPlugin):
-    plugins.implements(plugins.IConfigurer)
-    plugins.implements(plugins.ITemplateHelpers)
+  plugins.implements(plugins.IConfigurer)
+  plugins.implements(plugins.ITemplateHelpers)
 
-    # IConfigurer
+  # IConfigurer
 
-    def update_config(self, config_):
-        toolkit.add_template_directory(config_, 'templates')
-        toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('assets','trern_theme')
+  def update_config(self, config_):
+    toolkit.add_template_directory(config_, 'templates')
+    toolkit.add_public_directory(config_, 'public')
+    toolkit.add_resource('assets','trern_theme')
 
-    def get_helpers(self):
-      return {'trern_theme_get_groups': get_groups}
+  def get_helpers(self):
+    return {
+      'trern_theme_get_groups': get_groups,
+    }
